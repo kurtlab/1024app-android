e49f5271c669efab9d885d2161737766cc6479ec
kurtchen <cjx2328@gmail.com>
2019-10-15 09:01:36 +0800

    添加预计需要的各种类

---
+++ app/src/main/java/com/t66y/android/a1024app_android/modules/main/ui/activity/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.t66y.android.a1024app_android.modules.main.ui.activity;
+
+import android.app.Activity;
+
+/**
+ * Created by kurtchen on 2019/10/14.
+ */
+
+public class SplashActivity extends Activity{
+
+    protected void onCreate(){
+
+    }
+
+
+}
